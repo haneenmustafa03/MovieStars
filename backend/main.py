6c9925aafeba1ea81ba0d9988847f0d994787471
haneenmustafa03 <haneenmustafa@ufl.edu>
2024-11-29 15:09:30 -0500

    added function for calculating top 5 movies, updated index.html to add buttons and title

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request #flask will be used to create the web app its the bridge between front and backend, jsonify will convert lists into JSON format, request used to handle incoming requests
+from flask_cors import CORS #using cors in case we run into cross origin issues using live server for testing
 import random #to suggest 200 random genomes
 import csv #to read files
 
 app = Flask(__name__) #initializes appication instance 
+CORS(app) #add cors here, remove once app is actually launched
 
 #Our first data structure we are using is hashmaps
 def load_genome_tags():
@@ -11,6 +13,7 @@ def load_genome_tags():
         reader = csv.DictReader(file) #reader is an iterator that we will use to read in each line
         for row in reader: #going line by line in the code and parsing the data into hashmap
             genome_tags[int(row['tagId'])] = row["tag"] #convert the key value to an int here and set it to the tag
+
 
         return genome_tags #this will return the tagIds and values for use later
 
@@ -25,12 +28,8 @@ def load_genome_scores():
             relevanceFactor = float(row["relevance"])
 
             if movieId not in genome_scores:
-                genome_scores[movieId] = []
-            genome_scores[movieId].append({'genomeId' : genomeId, 'relevanceFactor' : relevanceFactor})
-
-        for movieId, genome_list in genome_scores.items():
-            sorted(genome_list, key=lambda x: x['relevanceFactor'], reverse=True) #key = lambda x: tells the sort function to sort based on relevanceFactor, reverse to make descending
-
+                genome_scores[movieId] = {}
+            genome_scores[movieId][genomeId] = relevanceFactor
     return genome_scores
 
 #load the movies from the csv files and their Ids in hashmap
@@ -51,16 +50,47 @@ def find_movie_suggestions(selected_tags, genome_scores, movies_and_genres, geno
     #iterate over all of the user selected genomes
     for tag in selected_tags:
         tagId = None
-        for id, name in genome_tags: #find the id number associated with the genome
+        for id, name in genome_tags.items(): #find the id number associated with the genome
             if tag == name:
                 tagId = id
                 break
 
     #pick up right here, somehow add up relevance factors and return top five movies
+        for movieId in genome_scores:
+            #adding up the relevance factors for all of the 
+            movie_relevance[movieId] = movie_relevance.get(movieId, 0) + genome_scores[movieId][tagId]
+    
+    #sort movie relevance factors in order to get the top five
+    sorted_movies = sorted(movie_relevance.items(), key=lambda x: x[1], reverse=True) #the lambda just means sort the relevance values in the hashmap
+
+    top_5_movies = sorted_movies[:5]
+
+    #Get the movie details and genres to return to the user
+    movie_details = []
+    for movie_id, score in top_5_movies:
+        movie = movies_and_genres.get(movie_id, {})
+        movie_details.append({
+            'title': movie['title'],
+            'genres': movie['genres'],
+            'relevance': score
+        })
+
+    return movie_details
+
+@app.route('/suggest_movies', methods=['POST']) #/sugges_movies is the path where the app will respond to requests, post means we expect data
+def suggest_movies():
+    data = request.json #converts data to json format
+    selected_tags = data.get('selected_tags', []) #pulls the selected tags from request
+    results = find_movie_suggestions(selected_tags, genome_scores, movies_and_genres, genome_tags)
+    return jsonify(results)
+
 
 genome_tags = load_genome_tags()
 genome_scores = load_genome_scores()
 movies_and_genres = load_movies()
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 #example accessing the load genome scores hashmap, id number - 1 because indexing starts at 0, and movie titles
