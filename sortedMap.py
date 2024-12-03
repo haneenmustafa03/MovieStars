ecdc519fc73ecb962168d13d542fe424bd42b2aa
isabellagoodwin970 <bellagoodwin@protonmail.com>
2024-12-03 16:57:04 -0500

    sortedMap class + added function

---
+++ sortedMap.py
@@ -0,0 +1,12 @@
+class SortedMap:
+    def __init__(self):
+        self.hashmap = {} #initialize empty dictionary for key-value pairs
+    def insert(self, key, value):
+        self.hashmap[key] = value #insert key-value pair into dictionary
+    def get(self, key, default= 0):
+        return self.hashmap.get(key, 0) #return value associated with key, changed none to default 0
+    def delete(self, key):
+        if key in self.hashmap:
+            del self.hashmap[key] #delete key-value pair
+    def items(self):
+        return sorted(self.hashmap.items()) #return all key-value pairs
